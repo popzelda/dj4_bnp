0bd355dce2eecdf700cd84c4f2d8316eb1f95391
popzelda <popzelda@gmail.com>
2023-06-04 01:38:48 +0100

    add apps

--- projet/settings.py
+++ projet/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'about',
+    'accounts',
+    'blog',
+    'propreties',
+    'setingss',
 ]
 
 MIDDLEWARE = [
---
+++ propreties/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PropretiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'propreties'
