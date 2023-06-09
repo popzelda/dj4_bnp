16be5169c2a25c42f53f9f86ae18f862f1975cc3
popzelda <popzelda@gmail.com>
2023-06-09 12:32:15 +0100

    just another check point

--- propreties/views.py
+++ propreties/views.py
@@ -4,9 +4,67 @@ from .models import *
 
 class PropretyList(ListView):
     model = Proprety
-    paginate_by = 1
+    paginate_by = 3
     ### filter
     ### paginatuin
+
 class PropretyDetail(DetailView):
     model = Proprety
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['related'] = Proprety.objects.filter(category=self.get_object().category)[:2]
+        return context
     ### book
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
